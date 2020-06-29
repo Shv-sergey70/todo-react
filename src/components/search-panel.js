f734d0eca94c94e7c4abe96daa708396c743b4c2
Sergey Shershnev <shv.sergey70@gmail.com>
2020-06-29 15:48:51 +0300

    init of react app

---
+++ src/components/search-panel.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './search-panel.css';
+
+const SearchPanel = () => {
+  return (
+    <input
+      className="form-control search-input"
+      placeholder="type to search"
+    />
+  )
+};
+
+export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppHeader from "./components/app-header";
+import TodoList from "./components/todo-list";
+import SearchPanel from "./components/search-panel";
+import ItemStatusFilter from "./components/item-status-filter";
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const todoData = [
+    {label: 'Drink Coffee', important: false, id: 1},
+    {label: 'Make Awesome App', important: true, id: 2},
+    {label: 'Have a lunch', important: false, id: 3},
+  ];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+     <div className="top-panel d-flex">
+       <SearchPanel />
+       <ItemStatusFilter />
+     </div>
+
+      <TodoList todos={todoData}/>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
