42d74094ac04083cf73c991f8cfa5a883659c4df
Sergey Shershnev <shv.sergey70@gmail.com>
2020-06-30 21:22:13 +0300

    added input for task of new item

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,15 +3,35 @@ import React, {Component} from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
-  render() {
-    const {onItemAdded} = this.props;
+  constructor(props) {
+    super(props);
 
+    this.state = {label: ''};
+
+     this.onLabelChange = (evt) => {
+       this.setState({label: evt.target.value});
+    };
+
+    this.onSubmit = (evt) => {
+      evt.preventDefault();
+
+      props.onItemAdded(this.state.label);
+
+      this.setState({label: ''});
+    }
+  }
+
+  render() {
     return (
-      <div className="item-add-form">
-        <button
-          className="btn btn-outline-secondary"
-          onClick={() => onItemAdded ('myText')}>Add Item</button>
-      </div>
+      <form className="item-add-form d-flex"
+            onSubmit={this.onSubmit}>
+        <input type="text"
+               className="form-control"
+               onChange={this.onLabelChange}
+               placeholder="What needs to be done"
+               value={this.state.label}/>
+        <button className="btn btn-outline-secondary">Add&nbsp;Item</button>
+      </form>
     )
   }
 }
