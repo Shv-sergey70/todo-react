c14882703a5f92f898ae8301850e0ff145ae56c2
Sergey Shershnev <shv.sergey70@gmail.com>
2020-08-14 19:31:49 +0700

    Updates

---
+++ precache-manifest.3be961e0ddfb59690c786fdd777f19dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffcdfc4b6db91896fd931e59afc1841",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "b239fb65187bcd4e45a8",
+    "url": "/todo-react/static/css/main.16c0ed59.chunk.css"
+  },
+  {
+    "revision": "64e1f52c14533b1e6c87",
+    "url": "/todo-react/static/js/2.18acf814.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.18acf814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b239fb65187bcd4e45a8",
+    "url": "/todo-react/static/js/main.40e35629.chunk.js"
+  },
+  {
+    "revision": "a049eaa3d15bfcc71b2e",
+    "url": "/todo-react/static/js/runtime-main.b204df10.js"
+  }
+]);
